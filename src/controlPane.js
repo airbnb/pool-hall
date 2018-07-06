1786bcc84ab8f4bdd67fe050c4ad1c95208acd17
Andy Fang <andy.fang@airbnb.com>
2018-07-06 03:00:48 -0700

    new interface

--- src/controlPane.js
+++ src/controlPane.js
@@ -1,10 +1,30 @@
 import express from 'express';
 
-function startControlPane(pool, port) {
+const DEFAULT_SETTINGS = {
+  port: 21474,
+  defaultPanes: [],
+};
+
+function startControlPane(pool, { port = DEFAULT_SETTINGS.port, panes = [] }) {
   const control = express();
   control.listen(port);
   pool.controlPane = control; // eslint-disable-line no-param-reassign
+  panes.concat(DEFAULT_SETTINGS.defaultPanes).forEach(pane => pane(pool, control));
   return control;
 }
 
-export default startControlPane;
+/**
+ * Starts an express server that can take
+ * @param {poolHall} pool     the pool instance
+ * @param {Object} settings
+ *        port:     port for the internal server to listen on
+ *        panes:    callbacks that takes in (pool, expressApp),
+ *                  and attaches express middlewares on the app
+ */
+export default function controlPane(pool, settings = {}) {
+  if (pool.isSupervisor) {
+    const control = startControlPane(pool, settings);
+    return control;
+  }
+  return null;
+}
